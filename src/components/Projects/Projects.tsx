b4b6bdc5f3df4e7226cf682745f4a2902c84e7a0
Anthony <AnthonyMercadante96@gmail.com>
2023-12-26 23:05:31 -0500

    Added placeholder pages.
    Added a placeholder page for work experience and a placeholder page for projects.

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      {/* Placeholder for projects content */}
+      <p>Details about projects will go here.</p>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WorkExperience = () => {
+  return (
+    <div>
+      <h1>Work Experience</h1>
+      {/* Placeholder for work experience content */}
+      <p>Details about work experience will go here.</p>
+    </div>
+  );
+};
+
+export default WorkExperience;
