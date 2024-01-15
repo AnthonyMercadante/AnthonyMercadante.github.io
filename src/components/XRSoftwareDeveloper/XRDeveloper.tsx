0472dad5c7f0a9c21150dae1524945a0eb17689d
Anthony <AnthonyMercadante96@gmail.com>
2024-01-14 19:16:14 -0500

    Consolidated import statements for XRDeveloper page.

--- src/components/XRSoftwareDeveloper/XRDeveloper.tsx
+++ src/components/XRSoftwareDeveloper/XRDeveloper.tsx
@@ -1,30 +1,30 @@
+// React and Hooks
 import React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'; // Back icon
-import IconButton from '@mui/material/IconButton';
-import { styled } from '@mui/material/styles';
 
+// Material UI Components, Icons, and Styles
+import { Box, Grid, Typography, IconButton, useTheme, styled } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'; // Back icon
+
+// Asset Imports
 import ProjectImage from '../../assets/images/OpenFlowIcon.png';
 import ProjectImage2 from '../../assets/images/CellTowerIcon.png';
 import ProjectImage3 from '../../assets/images/OVIN.png';
 
 
+
   const ProjectIcon = styled('img')(({ theme }) => ({
-    borderRadius: '20%', // Rounded corners for an icon-like appearance
-    width: '70%', // Reduced width for desktop
+    borderRadius: '20%', 
+    width: '70%', 
     height: 'auto',
     objectFit: 'cover',
     transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
     '&:hover': {
       transform: 'scale(1.05)',
-      boxShadow: '0 4px 20px 0 rgba(0, 0, 0, 0.2)', // Shadow effect on hover
+      boxShadow: '0 4px 20px 0 rgba(0, 0, 0, 0.2)', 
     },
     [theme.breakpoints.down('sm')]: {
-      width: '75%', // Increased width for mobile
+      width: '75%', 
     },
   }));
 
@@ -48,7 +48,6 @@ import ProjectImage3 from '../../assets/images/OVIN.png';
         route: '/OVIN',
         imageUrl: ProjectImage3,
       }
-      // ... other projects
     ];
   
   
@@ -80,7 +79,7 @@ import ProjectImage3 from '../../assets/images/OVIN.png';
               <Typography variant="subtitle1" sx={{ 
                 mt: 1, 
                 color: 'white',
-                fontSize: { xs: '0.7rem', sm: '1rem' }  // Smaller font size on extra-small screens
+                fontSize: { xs: '0.7rem', sm: '1rem' } 
               }}>
                 {project.title}
               </Typography>
