3e0591fb2128a84611fc76e5e65982941e213eb3
Anthony <AnthonyMercadante96@gmail.com>
2024-01-26 22:27:36 -0500

    Added Hero Icons, and adjusted About Me page.

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -1,13 +1,11 @@
-// AboutMe.tsx
 import React from 'react';
-import { Button, IconButton, Grid, Box, useTheme } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from 'react-router-dom';
 import profileImage from '../../assets/images/professional-photo.jpg';
 import LandscapeOverlay from '../../components/LandscapeOverlay';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Button from '@mui/material/Button';
 
 const AboutMe = () => {
-  const theme = useTheme();
   const navigate = useNavigate();
 
   const navigateToSkills = () => {
@@ -19,83 +17,60 @@ const AboutMe = () => {
   };
 
   return (
-    <Box
-      sx={{
-        backgroundColor: 'transparent',
-        color: theme.palette.text.primary,
-        minHeight: '100vh',
-        minWidth: '100vw',
-        padding: theme.spacing(4),
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontFamily: 'Roboto, Arial, sans-serif',
-        boxSizing: 'border-box',
-        overflow: 'hidden',
-        px: [2, 4, 8],
-        '@media (max-width: 932px)': {
-          // Adjustments for screens smaller than 932px
-          px: [1, 2, 4],
-          '& .about-me-profile-image': {
-            maxWidth: '75%', 
-          },
-          '& h1': {
-            fontSize: '1.5rem', 
-          },
-          '& p': {
-            fontSize: '0.9rem', 
-          },
-        },
-        '@media (max-width: 667px)': {
-          // Further adjustments for screens smaller than 667px
-          '& .about-me-profile-image': {
-            maxWidth: '70%', 
-          },
-          '& h1': {
-            fontSize: '1.25rem', 
-          },
-          '& p': {
-            fontSize: '0.8rem', 
-          },
-        },
-      }}
-    > 
-      <IconButton onClick={handleBack} sx={{ position: 'absolute', top: 20, left: 20, zIndex: 1 }}>
-        <ArrowBackIcon />
-      </IconButton>
-      <Grid container spacing={0} justifyContent="center" alignItems="center">
-        <Grid item xs={12} sm={6} md={4} lg={3}>
+    <div className="min-h-screen min-w-full bg-black text-gray-300 flex flex-col justify-center p-4">
+      <button onClick={handleBack} className="text-white absolute top-5 left-5 flex items-center">
+        <ArrowBackIcon /> {/* Material-UI Icon */}
+      </button>
+      <div className="flex flex-col sm:flex-row justify-center items-center sm:items-start">
+        <div className="w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 mb-4 sm:mb-0">
           <img
             src={profileImage}
             alt="Profile"
-            style={{
-              maxWidth: '100%',
-              height: 'auto',
-              borderRadius: '100px',
-              boxSizing: 'border-box',
-            }}
-            className='about-me-profile-image'
+            className="max-w-full h-auto rounded-full shadow-lg"
           />
-        </Grid>
-        <Grid item xs={12} sm={8} md={6} lg={5} style={{ textAlign: 'center', padding: theme.spacing(5) }}>
-          <h1>Hello! I'm Anthony Mercadante</h1>
-          <p>
-            Founder of Raethexn Technologies, specializing in custom software solutions.
-            I'm also a 3rd year Software Development student at Mohawk College,
-            driven by a lifelong passion for technology.
-          </p>
+        </div>
+        <div className="w-full sm:w-2/3 md:w-1/2 lg:w-2/5 text-center sm:text-left p-5">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-4">Hello! I'm Anthony Mercadante</h1>
+          <div className="space-y-4">
+            <div className="space-y-4">
+              <p className="transition duration-300 ease-in-out hover:text-gray-100 hover:shadow-md">
+                <span className="font-bold">I'm a software developer</span> with a love for technology that has been with me since my earliest days.
+                Currently in my 3rd year at <span className="italic text-blue-400">Mohawk College</span>, I am pursuing an advanced diploma in Software Development.
+              </p>
+              <p className="transition duration-300 ease-in-out hover:text-gray-100 hover:shadow-md">
+                My journey with technology began at the age of 14, when I started <span className="font-bold">programming as a hobby</span>.
+              </p>
+              <p className="transition duration-300 ease-in-out hover:text-gray-100 hover:shadow-md">
+                Back then, <span className="underline">coding was a personal pursuit</span>, driven by curiosity and the joy of creating things.
+              </p>
+              <p className="transition duration-300 ease-in-out hover:text-gray-100 hover:shadow-md">
+                What sets me apart in my educational and professional journey is my <span className="text-yellow-300">love for coding and technology</span>.
+                While many see it as a means to an end, for me, it's a pursuit of passion.
+              </p>
+              <p className="transition duration-300 ease-in-out hover:text-gray-100 hover:shadow-md">
+                My enthusiasm extends beyond the classroom and the workplace; I like to immerse myself in coding, discussing industry trends, and
+                constantly learning, even in my leisure time. This enthusiasm, I've realized, is <span className="italic">somewhat rare</span>.
+              </p>
+              <p className="transition duration-300 ease-in-out hover:text-gray-100 hover:shadow-md">
+                In essence, <span className="font-bold text-green-400">I am not just a software developer</span>; I am someone who lives and breathes technology.
+                Every line of code I write is a step towards getting better, a testament to my commitment to growth,
+                and an opportunity to contribute positively to the world. Welcome to my journey.
+              </p>
+            </div>
+
+          </div>
           <Button
             variant="contained"
             color="primary"
-            sx={{ marginTop: theme.spacing(2) }}
             onClick={navigateToSkills}
+            sx={{ marginTop: 2, backgroundColor: '#333', ':hover': { backgroundColor: '#555' } }}
           >
             View My Skills
           </Button>
-        </Grid>
-      </Grid>
+        </div>
+      </div>
       <LandscapeOverlay />
-    </Box>
+    </div>
   );
 };
 
