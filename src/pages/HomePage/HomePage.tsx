da5935ac0c9407e650a9999e111e1c34ed0f373e
Anthony <AnthonyMercadante96@gmail.com>
2025-05-27 15:59:22 -0400

    adjusted heading line.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -105,7 +105,7 @@ export default function HomePage() {
 
         {/* tagline */}
         <p className="text-center text-sm sm:text-base opacity-90">
-          Full-stack software developer. Crafting code, design, and sound.
+          Full-stack software developer.
         </p>
 
         {/* links */}
